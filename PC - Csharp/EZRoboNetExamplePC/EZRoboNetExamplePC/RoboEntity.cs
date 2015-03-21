fe9ddc3d4d3367139a0b39a97966b9312f02fdc8
terzakig <terzakig@hotmail.com>
2015-03-21 21:17:33 +0000

    Added GNU comment headers

--- PC - Csharp/EZRoboNetExamplePC/EZRoboNetExamplePC/RoboEntity.cs
+++ PC - Csharp/EZRoboNetExamplePC/EZRoboNetExamplePC/RoboEntity.cs
@@ -1,4 +1,26 @@
-﻿using System;
+﻿/////////////////// EZRobotNet ///////////////////////////////////
+//
+//			A RoboEntity class: This a class that implements a remote controller of a robot (hence the "avatar id" corresponds to ther espective drone robot in the field)
+//          The robo entity is essentially the "brain" of an MCU controlled drone:
+//                 RoboEntity.cs
+
+// A Network Protocol for Wireless/Wired Serial Communications for Cost Effective Robotic applications
+// 
+// Copyright (C) 2010 George Terzakis
+
+//	This program is free software; you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation; either version 2 of the License, or
+//	(at your option) any later version.
+
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//	GNU General Public License for more details.
+//	You should have received a copy of the GNU General Public License along
+//	with this program; if not, write to the Free Software Foundation, Inc.,
+//	51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
